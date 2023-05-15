b9ada0eeeae75b6d2f8cb3e4dc9ebf719fda2b0b
nathanlandau-ea <133191998+nathanlandau-ea@users.noreply.github.com>
2023-05-15 15:29:54 +0200

    added python code run

--- script.js
+++ script.js
@@ -13,3 +13,10 @@ window.onload = function () {
         chat,
     );
 }
+
+function executeCode(){
+    code = document.getElementsByTag("pre")[0].innerText
+    pyscript_element = document.createElement("py-script")
+    pyscript_element.innerText = code
+    document.getElementById("kruskal").append(pyscript_element)
+}
